ad966c376e6d30ed2fc42ef71f9c4e2c8ffa2e61
jonascicek <jonascicek98@gmail.com>
2024-10-02 15:40:29 +0200

    Add Function

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,1 +1,22 @@
 # python-shoppinglist
+
+# Leere Einkaufsliste
+shoppinglist = []
+
+# Funktion zum hinzufügen eines Artikels
+def add_item():
+    item = input("Welche Artikel sollen auf die Einkaufsliste hinzugefügt werden: ")
+    shoppinglist.append(item)
+    print(f"{item} wurde der Einkaufsliste hinzugefügt.")
+
+# Funktion zum Anzeigen der Einkaufsliste
+
+def show_shoppinglist():
+    if shoppinglist:
+        print("Deine Einkaufliste:")
+        for item in shoppinglist:
+            print(f"{item}")
+    else:
+        print("Die Einkaufsliste ist leer.")
+
+show_shoppinglist()
