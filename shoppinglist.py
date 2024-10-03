5a0f595ba85791e38533dbae2a61ef386628d492
jonascicek <jonascicek98@gmail.com>
2024-10-03 15:45:18 +0200

    Added main function

--- shoppinglist.py
+++ shoppinglist.py
@@ -19,4 +19,24 @@ def show_shoppinglist():
     else:
         print("Die Einkaufsliste ist leer.")
 
-show_shoppinglist()
+# Haupt Funktion
+
+def main():
+    while True:
+        print("-----Einkaufsliste-----")
+        print("1. Artikel zur Einkaufsliste hinzufügen")
+        print("2. Einkaufsliste anzeigen")
+        print("3. Programm beenden")
+
+    # Speichere die Eingabe des Benutzers in der Variable choice
+        choice = input("Gib deine Wahl ein (1/2/3)")
+        if choice == "1":
+            add_item()
+        elif choice == "2":
+            show_shoppinglist()
+        elif choice == "3":
+            print("Programm wird beendet! Auf Wiedersehen")
+            break
+        else:
+            print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3")
+main()
